85f181e46fdcfd47778d1d46a1b88ce33fb9e1ed
spidernak <127380223+spidernak@users.noreply.github.com>
2024-03-25 17:40:08 +0700

    first

---
+++ src/main/java/itc/com/App.java
@@ -0,0 +1,13 @@
+package itc.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
