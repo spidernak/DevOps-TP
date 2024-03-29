ee5a3c365acc1520ec9d52c1266ab8705b4b6720
spidernak <127380223+spidernak@users.noreply.github.com>
2024-03-29 16:27:51 +0700

    midterm

---
+++ src/main/java/midterm/exam/midterm/ServletInitializer.java
@@ -0,0 +1,13 @@
+package midterm.exam.midterm;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MidtermApplication.class);
+	}
+
+}
---
+++ src/test/java/midterm/exam/midterm/MidtermApplicationTests.java
@@ -0,0 +1,13 @@
+package midterm.exam.midterm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidtermApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
